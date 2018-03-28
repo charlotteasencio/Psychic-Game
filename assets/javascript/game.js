dfe23192f557ca9f3b986450730bdf6ef6981058
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-03-28 19:56:34 -0400

    added random computer choice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,26 @@
-$(document).ready(function() {
-
 var letterChoices = ["a", "b" , "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
     "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var wins
-var losses
-var guessesLeft
-var guessesSoFar
-var computerGuess
-var userGuess
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = 0;
+var userGuess = [];
 
+//computer chooses a random letter
+var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+
+//user chooses a letter
+
+//check if the two match 
+
+if(userGuess === computerGuess) {
+    wins++;
+    guessesLeft = 9;
+    guessesSoFar = 0;
+} else {
+    guessesLeft--;
+    //add user guess to user guess array
+}
+
+//loop until guessesLeft = 0 then reset guessessLeft, guessesSoFar, and losses++
+//run loop again
