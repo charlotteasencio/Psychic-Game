eb75311480f9f6cacbd6181154e9f4aa9ccd7551
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-03-28 19:30:56 -0400

    added variables and initial javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+var letterChoices = ["a", "b" , "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
+    "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins
+var losses
+var guessesLeft
+var guessesSoFar
+var computerGuess
+var userGuess
+
