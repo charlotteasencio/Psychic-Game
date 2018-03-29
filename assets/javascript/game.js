cc09899d6023c60dc40e3e6b7ff30a0be30271d1
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-03-28 23:41:13 -0400

    computer guess and user guess working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,36 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar = 0;
-var userGuess = [];
 
 //computer chooses a random letter
 var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+    console.log(computerGuess);
 
 //user chooses a letter
-
-//check if the two match 
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+    console.log(userGuess);
 
 if(userGuess === computerGuess) {
     wins++;
-    guessesLeft = 9;
-    guessesSoFar = 0;
-} else {
+}else{
     guessesLeft--;
-    //add user guess to user guess array
 }
 
+if(guessesLeft === 0) {
+    losses++
+    }
+
+
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "losses: " + losses;
+document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+
+}
+
+//add letter to useGuess array
+
+
 //loop until guessesLeft = 0 then reset guessessLeft, guessesSoFar, and losses++
+
 //run loop again
