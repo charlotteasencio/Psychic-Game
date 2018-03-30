be0cdbcf4279bde77817d05538a9d53e334b27f4
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-03-30 17:48:07 -0400

    user guesses adding to new array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,9 @@ var letterChoices = ["a", "b" , "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guessesSoFar = 0;
+var guessesSoFar = [];
 
+for (i = 0; guessesLeft > 0; i++) {
 //computer chooses a random letter
 var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
     console.log(computerGuess);
@@ -12,28 +13,27 @@ var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.lengt
 //user chooses a letter
 document.onkeypress = function(event) {
     var userGuess = event.key;
-    console.log(userGuess);
 
+//user guess matches computer guess
 if(userGuess === computerGuess) {
     wins++;
+    guesses = 9;
+//user guess does not match computer guess
 }else{
     guessesLeft--;
+    guessesSoFar.push(userGuess);
 }
-
+//guessesLeft equals zero but user guess never equaled computer guess
 if(guessesLeft === 0) {
-    losses++
+    losses++;
+    guessesLeft = 9;
+    //start loop over, get computer to choose another letter
     }
 
-
+//change html elements according to guesses, wins, losses
 document.getElementById('wins').innerHTML = "Wins: " + wins;
 document.getElementById('losses').innerHTML = "losses: " + losses;
 document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
-
+document.getElementById("guessesSoFar").innerHTML= "Your Guesses so Far: " + guessesSoFar.join(", ");
+    }
 }
-
-//add letter to useGuess array
-
-
-//loop until guessesLeft = 0 then reset guessessLeft, guessesSoFar, and losses++
-
-//run loop again
